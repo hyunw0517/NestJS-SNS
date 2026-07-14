5346aa7331bc18ff02475b917fecc38548a7226d
최현우 <hyunw0517@naver.com>
2026-07-14 22:04:16 +0900

    Controller 비즈니스 로직 Service로 분리

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,64 +1,23 @@
 import { Body, Controller, Delete, Get, NotFoundException, Param, Post, Put } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
-// 게시글 작성용 인터페이스
-interface PostModel {
-  id: number;
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
-
-let posts : PostModel[] = [
-  {
-    id: 1,
-    author: 'newyork_official',
-    title: '스파이더맨',
-    content: '모두의 친구 스파이더맨',
-    likeCount: 1000000,
-    commentCount: 10000
-  },
-  {
-    id: 2,
-    author: 'stark_official',
-    title: '아이언맨',
-    content: '사람을 구하는 아이언맨',
-    likeCount: 1000000,
-    commentCount: 10000
-  },
-  {
-    id: 3,
-    author: 'usa_official',
-    title: '캡틴아메리카',
-    content: '미국을 상징하는 캡틴아메리카',
-    likeCount: 1000000,
-    commentCount: 10000
-  }
-];
-
 @Controller('posts')
 export class PostsController {
-  constructor(private readonly postsService: PostsService) {}
+
+  constructor(private readonly postsService: PostsService) {
+
+  }
 
   //* 1) GET /posts        -> 리스트 조회
   @Get()
   getPosts(){
-    return posts;
+    return this.postsService.getAllPosts();
   }
 
   //* 2) GET /posts/:id    -> id에 해당하는 post 조회
   @Get(':id')
   getPost(@Param('id') id: string){ //별도 작업을 안하는 이상 파라미터는 string이 기본값. 
-    const post = posts.find((post) => post.id === +id); // +id -> Number(id) 대체 가능
-
-    //오류 처리 : 데이터를 찾지 못할 경우 404 에러
-    if( !post ){
-      throw new NotFoundException();
-    }
-
-    return post;
+    return this.postsService.getPostById(+id);
   }
 
   //* 3) POST /posts       -> post 생성
@@ -68,22 +27,7 @@ export class PostsController {
     @Body('title') title: string,
     @Body('content') content: string
   ){
-    const post = {
-      id: posts[posts.length - 1].id + 1,
-      author, //타입 명 동일하면 생략가능
-      title, 
-      content, 
-      likeCount: 0,
-      commentCount: 0
-    };
-
-    // 기존 posts 배열에 신규 post 추가
-    posts = [
-      ...posts,
-      post
-    ];
-    
-    return post;
+    return this.postsService.createPost(author, title, content);
   }
 
   //* 4) PUT /posts/:id    -> id에 해당하는 post 변경
@@ -94,25 +38,7 @@ export class PostsController {
     @Body('title') title?: string,
     @Body('content') content?: string
   ){
-    const post = posts.find((post) => post.id === +id);
-
-    if(!post){
-      throw NotFoundException;
-    }
-
-    if(author){
-      post.author = author;
-    }
-    if(title){
-      post.title = title;
-    }
-    if(content){
-      post.content = content;
-    }
-
-    posts = posts.map(prevPost => prevPost.id === +id ? post : prevPost);
-
-    return post;
+    return this.postsService.updatePost(+id, author, title, content);
   }
 
   //* 5) DELETE /posts/:id -> id에 해당하는 post 삭제
@@ -120,15 +46,7 @@ export class PostsController {
   deletePost(
     @Param('id') id: string
   ){
-    const post = posts.find((post) => post.id === +id);
-
-    if(!post){
-      throw NotFoundException;
-    }
-    
-    posts = posts.filter(post => post.id !== +id);
-
-    return id;
+    return this.postsService.deletePost(+id);
   }
 
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,117 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+// 게시글 작성용 인터페이스
+export interface PostModel {
+  id: number;
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+let posts : PostModel[] = [
+  {
+    id: 1,
+    author: 'newyork_official',
+    title: '스파이더맨',
+    content: '모두의 친구 스파이더맨',
+    likeCount: 1000000,
+    commentCount: 10000
+  },
+  {
+    id: 2,
+    author: 'stark_official',
+    title: '아이언맨',
+    content: '사람을 구하는 아이언맨',
+    likeCount: 1000000,
+    commentCount: 10000
+  },
+  {
+    id: 3,
+    author: 'usa_official',
+    title: '캡틴아메리카',
+    content: '미국을 상징하는 캡틴아메리카',
+    likeCount: 1000000,
+    commentCount: 10000
+  }
+];
+
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+
+    //* GET
+    getAllPosts(){
+        return posts;
+    }
+
+    //* GET(:id)
+    getPostById(id: number){
+        const post = posts.find((post) => post.id === +id); // +id -> Number(id) 대체 가능
+    
+        //오류 처리 : 데이터를 찾지 못할 경우 404 에러
+        if( !post ){
+            throw new NotFoundException();
+        }
+    
+        return post;
+    }
+
+    //* POST
+    createPost(author: string, title: string, content: string){
+        const post = {
+            id: posts[posts.length - 1].id + 1,
+            author, //타입 명 동일하면 생략가능
+            title, 
+            content, 
+            likeCount: 0,
+            commentCount: 0
+        };
+    
+        // 기존 posts 배열에 신규 post 추가
+        posts = [
+            ...posts,
+            post
+        ];
+        
+        return post;
+    }
+
+    //* PUT(:id)
+    updatePost(postId: number, author?: string, title?: string, content?: string){
+        const post = posts.find((post) => post.id === postId);
+
+        if(!post){
+            throw NotFoundException;
+        }
+
+        if(author){
+        post.author = author;
+        }
+        if(title){
+        post.title = title;
+        }
+        if(content){
+        post.content = content;
+        }
+
+        posts = posts.map(prevPost => prevPost.id === postId ? post : prevPost);
+
+        return post;
+    }
+
+    //* DELETE(:id)
+    deletePost(postId: number){
+        const post = posts.find((post) => post.id === postId);
+
+        if(!post){
+            throw NotFoundException;
+        }
+        
+        posts = posts.filter(post => post.id !== postId);
+
+        return postId;
+    }
+
+}
